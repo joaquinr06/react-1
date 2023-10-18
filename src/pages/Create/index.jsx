2d5af32f4fe027fbea7aa5d184862ba6e51b6d08
joaquinr06 <joaquingay06@gmail.com>
2023-10-18 00:37:24 -0300

    corrigiendo cosas

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,85 +1,150 @@
-import "./style.css";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import './style.css'
 
 function Create() {
-  
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
-  const [discount, setDiscount] = useState("");
-  const [category, setCategory] = useState("");
-  const [message, setMessage] = useState("");
+    const {
+        register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    reset
 
+    } = useForm({
+        defaultValues: {
+            name: "",
+            price: "",
+            discount: "",
+            description: "",
+            image: "",
+            colors: "",
+            category: ""
+        },
+    });
+    console.log(errors);
 
+    const requestOptions = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ tittle: 'Product'})
+    };
+    const onSubmit = async () => {
+        try {
+            await fetch(`http://localhost:3001/products/crear`, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                console.log(data);
+    
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }  reset();
 
-  let handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-       fetch("http://localhost:3001/products/crear", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          name: name,
-          price: price,
-          description: description,
-          discount: discount,
-          category: category
-        }),
-      });
-      let res = await res.json();
-      if (res.status === 200) {
-        setName("");
-        setPrice("");
-        setMessage("Producto creado correctamente");
-      } else {
-        setMessage("¡Algo ocurrió!");
-      }
-    } catch (err) {
-      console.log(err);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+        } catch (errors) {
+            console.log(errors);
+        }
     }
-  };
 
-  return (
-    <div className="Create">
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text"
-          value={name}
-          placeholder="Nombre"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input 
-          type="text"
-          value={price}
-          placeholder="Precio"
-          onChange={(e) => setPrice(e.target.value)}
-        />
-        <input 
-          type="text"
-          value={description}
-          placeholder="Descripcion"
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <input 
-          type="text"
-          value={discount}
-          placeholder="Descuento"
-          onChange={(e) => setDiscount(e.target.value)}
-        />
-        <input 
-          type="text"
-          value={category}
-          placeholder="Categoria"
-          onChange={(e) => setCategory(e.target.value)}
-        />
-        <button type="submit">Crear Producto</button>
+<form onSubmit={handleSubmit(onSubmit)} />
 
-        <div className="message">{message ? <p>{message}</p> : null}</div>
-      </form>
-    </div>
-  );
+
+return (
+
+    <
+        form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+            <label>Nombre:</label>
+            <input
+                type="string"
+                name="name"
+                {...register("name", {
+                    required: {
+                        value: true,
+                        message: "Nombre es requerido",
+                    },
+                    maxLength: 20,
+                    minLength: 2,
+                })}
+            />
+        </div>
+
+        {/* precio */}
+        <div>
+            <label htmlFor="price">Precio</label>
+            <input
+                type="number"
+                name="name"
+                {...register("name", {
+                    required: {
+                        value: true,
+                        message: "Nombre es requerido",
+                    },
+                    maxLength: 20,
+                    minLength: 2,
+                })}
+            />  
+        </div>
+        {/* Descuento */}
+        <div>
+            <label htmlFor="discount">Descuento</label>
+            <input
+                type="number"
+                name="discount"
+                {...register("discount")}
+            />
+        </div>
+        {/* Descripcion */}
+        <div>
+            <label htmlFor="description">Descripcion</label>
+            <input
+                type="string"
+                name="description"
+                {...register("description")}
+            />
+        </div>
+        {/* category */}
+        <div>
+            <label htmlFor="category">Categoria</label>
+            <input
+                type="string"
+                name="category"
+                {...register("category")}
+            />
+        </div>
+        {/* image */}
+        <div>
+            <label htmlFor="image">subir nombre de archivo:</label>
+            <input
+                type="file"
+                onChange={(e) => {
+                    setValue("image", e.target.files[0].name);
+                }}
+                {...register("image")}
+            />
+            {errors.archivo && <span>{errors.archivo.message}</span>}
+        </div>
+
+        {/* Color */}
+        <div>
+            <label htmlFor="colors">Color</label>
+            <input
+                type="string"
+                name="colors"
+                {...register("colors")}
+            />
+        </div>
+
+        <button type="submit">Enviar</button>
+
+    </form>
+)
 }
-
 
 export default Create;
